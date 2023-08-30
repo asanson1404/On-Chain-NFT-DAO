f1a890dff1bae1982cbb4b972f29ff8c0f27cf1c
asanson <alexandresanson.pro@gmail.com>
2023-08-30 23:10:23 +0200

    Definition of the struct for the DAO's proposals + writing of the SC constructor

--- hardhat/contracts/XelaDAO.sol
+++ hardhat/contracts/XelaDAO.sol
@@ -9,7 +9,30 @@ import "../interfaces/IXelaCollectionNFT.sol";
 
 contract XelaDAO is Ownable {
 
+    // Variable to store the contracts we will call
+    IFakeNFTMarketplace nftMarketplace;
+    IXelaCollectionNFT  xelaNFT;
 
+    // Struct containing all relevant information of a new proposal of purchase
+    struct Proposal {
+        uint256 tokenIdToBuy;
+        uint256 yesVote;
+        uint256 noVote;
+        uint256 deadline; // The UNIX timestamp until which this proposal is active. Proposal can be executed after the deadline has been exceeded.
+        bool    executed; // Whether or not this proposal has been executed. Cannot be executed before the deadline has been exceeded.
+        mapping(uint256 => bool) voters; // Mapping between XelaNFT tokenIDs and booleans to know if the NFT has been used to vote 
+    }
+
+    // Mapping of ID to Proposal in order to be able to list the proposals
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public numProposals;
+
+    // Payble constructor to accept ether from the deployer (to fill the DAO's treasury)
+    // Initializes the contract instances for the SC we will use
+    constructor(address _nftMarketplaceAdd, address _xelaNFTAdd) payable {
+        nftMarketplace = IFakeNFTMarketplace(_nftMarketplaceAdd);
+        xelaNFT        = IXelaCollectionNFT(_xelaNFTAdd);
+    } 
 
 
 }
