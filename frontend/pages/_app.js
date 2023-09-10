28130b210c03084757caeb92ad55447bd46192cd
asanson <alexandresanson.pro@gmail.com>
2023-09-10 13:04:23 +0200

    Set up the wallet connection with Rainbowkit : Rainbowkit installation doc followed

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,31 @@
-import '@/styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import '@/styles/globals.css';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import { goerli, sepolia } from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains([goerli, sepolia], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "Xela DAO",
+  projectId: "6e8e88ca2d45e9790c40e82dbe4462a5",
+  chains: chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
