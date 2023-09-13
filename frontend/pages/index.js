fca7f36fab75dafa44ba3c1ca768e49263a6a580
asanson <alexandresanson.pro@gmail.com>
2023-09-13 18:05:04 +0200

    First tests passed : Possible to be whitelisted + Add the payble fonctionnality for the mint........

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,7 @@
-import { XelaCollectionNFTAddress, XelaCollectionNFTABI, XelaDAOAddress, XelaDAOABI } from '../constants'
+import {  XelaCollectionNFTAddress, XelaCollectionNFTABI, 
+          XelaDAOAddress, XelaDAOABI,
+          WhitelistAddress, WhitelistABI, 
+} from '../constants'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
@@ -18,10 +21,40 @@ export default function Home() {
   // State variable to know if the Component has been mounted yet or not
   const [isMounted, setIsMounted] = useState(false);
 
+  // State variable to show loading state when waiting for a transaction to go through
+  const [loading, setLoading] = useState(false);
+
+  // State variable to show loading state when waiting for a transaction to go through
+  const [minting, setMinting] = useState(false);
+
+  // State variable to show loading state when waiting for a transaction to go through
+  const [joining, setJoining] = useState(false);
+
   // Number of Xela Collection NFTs
   const maxTokenIds = 14;
 
   // Fetch the number of reserved tokens
+  const nbTokenMinted = useContractRead({
+    ddress: XelaCollectionNFTAddress,
+    abi: XelaCollectionNFTABI,
+    functionName: 'totalSupply',
+  })
+
+  // Fetch the number of reserved tokens (whitelisted tokens)
+  const reservedTokens = useContractRead({
+    address: XelaCollectionNFTAddress,
+    abi: XelaCollectionNFTABI,
+    functionName: 'reservedTokens',
+  })
+
+  // Fetch the number of reserved tokens claimed (whitelisted tokens claimed)
+  const reservedTokensClaimed = useContractRead({
+    address: XelaCollectionNFTAddress,
+    abi: XelaCollectionNFTABI,
+    functionName: 'reservedTokensClaimed',
+  })
+
+  // Fetch the number of NFT the user owns
   const nftUserBalance = useContractRead({
     address: XelaCollectionNFTAddress,
     abi: XelaCollectionNFTABI,
@@ -40,6 +73,46 @@ export default function Home() {
   const daobalance = useBalance({
     address: XelaDAOAddress,
   });
+
+  // JSX - Function to join the whitelist (only 4 seats available)
+  async function joinWhitelist() {
+
+    setJoining(true);
+
+    try {
+      const hash = await writeContract({
+        address: WhitelistAddress,
+        abi: WhitelistABI,
+        functionName: 'addAddressToWhitelist',
+      });
+      await waitForTransaction(hash);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+
+    setJoining(false);
+  }
+
+  // JSX - Function to mint a Xela NFT
+  async function mintXelaNFT() {
+
+    setMinting(true);
+
+    try {
+      const hash = await writeContract({
+        address: XelaCollectionNFTAddress,
+        abi: XelaCollectionNFTABI,
+        functionName: 'mint',
+      });
+      await waitForTransaction(hash);
+    } catch (error) {
+      console.error(error);
+      window.alert(error);
+    }
+
+    setMinting(false);
+  }
 
 
   useEffect(() => {
@@ -67,20 +140,61 @@ export default function Home() {
 
       <div className={styles.main}>
         <div>
-          <h1 className={styles.title}>Welcome to Xela DAO</h1>
-          {console.log(Number(nftUserBalance.data))}
-          {console.log(address)}
-          <div className={styles.description}>Your Xela NFT balance: {Number(nftUserBalance.data)}<br/>
+          <h1 className={styles.title}>Welcome to <span className={styles.xelaDaoColor}>Xela DAO</span></h1>
+          <h3 className={styles.walletaddr}>Wallet address: {address.toString()}</h3>
+          <div className={styles.userBalanceDiv}>
+            Your Xela NFT balance: {Number(nftUserBalance.data)}<br/>
+            <div>
+              {reservedTokens.data !== reservedTokensClaimed.data ? (
+                <>
+                  {joining ? (
+                    <button className={styles.boutonAligne}>Joining...</button>
+                  ) : (
+                    <button className={styles.boutonAligne} onClick={() => joinWhitelist()}>Join Whitelist</button>
+                  )}
+                  {minting ? (
+                    <button className={styles.boutonAligne}>Minting...</button>
+                  ) : (
+                    <button className={styles.boutonAligne} onClick={() => mintXelaNFT()}>Mint Xela NFT</button>
+                  )}
+                </>
+              ) : (
+                <>
+                  {minting ? (
+                    <button className={styles.boutonSeul}>Minting...</button>
+                  ) : (
+                    <button className={styles.boutonSeul} onClick={() => mintXelaNFT()}>Mint Xela NFT</button>
+                  )}
+                </>
+              )}
+            </div>
+
+
+
+            
+          </div>
+            {
+              Number(nftUserBalance.data) === 0 && Number(nbTokenMinted.data) !== maxTokenIds && (
+                <div>
+                  {loading ? (
+                    <button >Minting...</button>
+                  ) : (
+                    <button>
+                      Mint Xela NFT
+                    </button>
+                  )}
+                </div>
+              )
+            }
             {daobalance.data && (
               <>
-                Treasury Balance:{" "}
+                DAO's Treasury Balance:{" "}
                 {formatEther(daobalance.data.value).toString()} ETH
+                RESERVED TOKENS = {reservedTokens.data}
+                RESERVED TOKENS CLAIMED = {reservedTokensClaimed.data}
               </>
             )} <br/>
-          </div>
-          <div>
-              Wallet address: {address.toString()}
-          </div>
+
 
 
         </div>
