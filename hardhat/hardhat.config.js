b1e8c4b7850a0510d5c3c5770d744f983afa6cfb
asanson <alexandresanson.pro@gmail.com>
2023-09-16 15:55:05 +0200

    Launch on Sepolia ready

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    goerli: {
+    sepolia: {
       url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
